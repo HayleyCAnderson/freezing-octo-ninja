7b01e36a188ba50f2c6828fce3b0f27a69299592
Hayley <hayleyanderson@zoho.com>
2014-11-04 11:00:18 -0500

    Create CARDS_PER_HAND global variable

--- player.rb
+++ player.rb
@@ -2,13 +2,15 @@ class Player
   attr_reader :name
   attr_accessor :hand
 
+  CARDS_PER_HAND = 5
+
   def initialize(number)
     @name = "Player " + number.to_s
     @hand = []
   end
 
   def get_hand(deck)
-    5.times do
+    CARDS_PER_HAND.times do
       @hand << deck.pop
     end
   end
